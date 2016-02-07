9212234dd5d5e6588b3826a009fd9e5000d54edf
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-02-07 20:12:32 +1300

    Changed players to a list and added comments

--- app/src/main/java/Game/Game.java
+++ app/src/main/java/Game/Game.java
@@ -1,5 +1,9 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 import Pieces.Piece;
 
 /**
@@ -9,7 +13,7 @@ import Pieces.Piece;
 
 public class Game{
 
-    private Player[] players;
+    private ArrayList<Player> players;
     private int CURRENT_PLAYER;
     private Map map;
 
@@ -27,7 +31,8 @@ public class Game{
         if(p.length < 1)
             throw new IllegalArgumentException("Need at least one player");
 
-        players = p;
+        players = new ArrayList<Player>(Arrays.asList(p));
+
         CURRENT_PLAYER = 0;
         map = m;
     }
@@ -36,40 +41,75 @@ public class Game{
 //Methods
 //---------------------------------------------------------------
 
+    /**
+     * Sets the current player to the next one in the array.
+     * Returns to the start of the array, if it would overflow.
+     */
     public void nextPlayer(){
-        CURRENT_PLAYER = (CURRENT_PLAYER+1)%players.length;
+        CURRENT_PLAYER = (CURRENT_PLAYER+1)%players.size();
     }
 
+    /**
+     * Moves the given piece one tile in the given direction.
+     *
+     * @param p The piece to move
+     * @param d The direction to move the piece.
+     * @return Returns true if the piece was succesfully moved.
+     */
     public boolean movePiece(Piece p, Direction d){
         //If the piece is owned by the current player.
         //It is allowed to be moved.
-        if(p.getOwnerID() == players[CURRENT_PLAYER].getID()){
+        if(p.getOwnerID() == players.get(CURRENT_PLAYER).getID()){
             return map.movePiece(p, d);
         }
         return false;
     }
 
-    public boolean removePlayer(Player p){
+    /**
+     * Removes a player from turn rotation. Will return an exception if the player is not in the game.
+     * @param p The player to remove
+     */
+    public void removePlayer(Player p){
         //TODO
-        int indexOfPlayer = 0;//Index of the player in players
+        int indexOfPlayer = players.indexOf(p);//Index of the player in players
+
+        //If the given player is not playing the game, throw an exception.
+        if(indexOfPlayer < 0)
+            throw new NoSuchElementException("Player not found");
+
+
+        //If the player is before the current player in the list, decrease the lists count by one.
         if(indexOfPlayer <= CURRENT_PLAYER)
             CURRENT_PLAYER--;
-        //Remove the player entry.
-        return true;
+
+        players.remove(p);
     }
 
-    public boolean addPlayer(Player p){
-        return true; //TODO
+    /**
+     * Adds the given player to the end of the current player cycle.
+     * @param p
+     * @return
+     */
+    public void addPlayer(Player p){
+        players.add(p);
     }
 
 //---------------------------------------------------------------
 //Getters and Setters
 //---------------------------------------------------------------
 
+    /**
+     * Returns the current player.
+     * @return
+     */
     public Player getCurrentPlayer(){
-        return players[CURRENT_PLAYER];
+        return players.get(CURRENT_PLAYER);
     }
 
+    /**
+     * Returns the map of the current game.
+     * @return
+     */
     public Map getMap(){
         return map;
     }
